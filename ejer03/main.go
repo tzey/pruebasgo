58d969d380fbf1f59402f83bdf3f6f5f85b2105d
Chema Galvez <chemagalvez@gmail.com>
2021-08-09 20:53:36 +0200

    fix

---
+++ ejer03/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+var estado bool
+
+func main() {
+	estado = false
+
+	if estado == true {
+		fmt.Println(estado)
+	} else {
+		fmt.Println(estado)
+	}
+
+}
--- ejercicio1/main.go
+++ ejercicio1/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 var numero int
 var texto1 string
@@ -9,9 +12,13 @@ var status1 bool = true
 func main() {
 	numero2, numero3, numero4, texto, status := 2, 5, 67, "Este es mi texto", false
 
-	var moneda int64 = 0
+	var moneda int64 = 21
 
 	numero2 = int(moneda)
+
+	texto = fmt.Sprintf("%d", moneda)
+
+	texto = strconv.Itoa(numero2)
 
 	fmt.Println(numero2)
 	fmt.Println(texto1)
